7862bdf545567632dbecd607fb12844cecf89475
Marko Marković <1478860+mmarkovic@users.noreply.github.com>
2021-05-14 10:12:52 +0200

    Improved performance by parallel code execution.

--- src/ImageProcessing/ImageProcessing/NumbersBySignature/ImageProcessor.cs
+++ src/ImageProcessing/ImageProcessing/NumbersBySignature/ImageProcessor.cs
@@ -1,6 +1,8 @@
 ﻿namespace ImageProcessing.NumbersBySignature
 {
     using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Threading.Tasks;
 
     public static class ImageProcessor
     {
@@ -12,19 +14,51 @@
 
             var bmp = new Bitmap(image);
 
-            for (int y = 0; y < bmp.Height; y++)
-            {
-                for (int x = 0; x < bmp.Width; x++)
-                {
-                    var oldColor = bmp.GetPixel(x, y);
-                    var averageColorValue = (oldColor.R + oldColor.G + oldColor.B) / 3;
-                    var newColor = averageColorValue > thresholdValue ? Color.White : Color.Black;
+            var rectangle = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bitmapData = bmp.LockBits(rectangle, ImageLockMode.ReadWrite, bmp.PixelFormat);
 
-                    bmp.SetPixel(x, y, newColor);
-                }
+            int bytesPerPixel = Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+            unsafe
+            {
+                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
+
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        var oldColor = GetColor(currentLine, x);
+                        var averageColorValue = (oldColor.R + oldColor.G + oldColor.B) / 3;
+
+                        var newColor = averageColorValue > thresholdValue ? Color.White : Color.Black;
+
+                        SetColor(currentLine, x, newColor);
+                    }
+                });
             }
 
+            bmp.UnlockBits(bitmapData);
+
             return bmp;
+        }
+
+        private static unsafe Color GetColor(byte* currentLine, int x)
+        {
+            return Color.FromArgb(
+                currentLine[x + 2],
+                currentLine[x + 1],
+                currentLine[x]);
+        }
+
+        private static unsafe void SetColor(byte* currentLine, int x, Color c)
+        {
+            currentLine[x + 2] = c.R;
+            currentLine[x + 1] = c.G;
+            currentLine[x] = c.B;
         }
     }
 }
