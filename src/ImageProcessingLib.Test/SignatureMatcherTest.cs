b8e6977d5f26033461dc81ef390ecca294448b4d
Marko Marković <1478860+mmarkovic@users.noreply.github.com>
2021-06-06 17:33:18 +0200

    Testing: Removed writing to disk in unit tests.

--- src/ImageProcessingLib.Test/SignatureMatcherTest.cs
+++ src/ImageProcessingLib.Test/SignatureMatcherTest.cs
@@ -1,9 +1,5 @@
 ﻿namespace ImageProcessingLib
 {
-    using System;
-    using System.Drawing;
-    using System.IO;
-
     using FluentAssertions;
 
     using ImageProcessingLib.TestData;
@@ -48,11 +44,6 @@
             var signature01Image = BinaryImage.FromImage(TestDataResources.calculatedSign360_01);
             var template01Image = BinaryImage.FromImage(TestDataResources.signatureTemplate360_01);
             var template02Image = BinaryImage.FromImage(TestDataResources.signatureTemplate360_02);
-
-            TestDataResources.calculatedSign360_01.Save(@"C:\devNull\output\original_signature01.png");
-            signature01Image.ToBitmap().Save(@"C:\devNull\output\test_signature01.png");
-            template01Image.ToBitmap().Save(@"C:\devNull\output\test_template01Image.png");
-            template02Image.ToBitmap().Save(@"C:\devNull\output\test_template02Image.png");
 
             bool firstResult = SignatureMatcher.IsMatch(signature01Image, template01Image);
             bool secondResult = SignatureMatcher.IsMatch(signature01Image, template02Image);
